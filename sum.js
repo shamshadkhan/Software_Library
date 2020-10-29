1205482181e6862552291958547288df0bb7b5c5
shamshad akther <shamshad.akther@tuni.fi>
2020-10-29 07:58:03 +0200

    update on coverage

--- sum.js
+++ sum.js
@@ -5,11 +5,26 @@
  * @returns {number} Returns the total.
  */
 
- function sum(augend, addend){
-     return augend + addend
- }
+const add = function (num1, num2) {
+
+    if ( Array.isArray(num1) ){
+      let sum = 0;
+      for ( let i = 0; i < num1.length; i++){
+        sum += num1[i];
+      }
+      return sum;
+    }
+    else if ( isNaN(num1, num2) ){
+      return NaN
+    }
+  
+    return parseFloat(num1) + parseFloat(num2);
+  
+  }
+  
+  export default add
 
  // node 12 and up
- export default sum
+//  export default sum
  // below for node 10
  //module.exports = sum
--- test/sum.test.js
+++ test/sum.test.js
@@ -8,4 +8,16 @@ describe("Sum", () => {
  it("adds positive number to positive number", () =>{
      expect(sum(3,6)).to.equal(9)
  });
+ it("adds positive number to negative number", () =>{
+    expect(sum(3,-6)).to.equal(-3)
+});
+it("adds negative number to negative number", () =>{
+    expect(sum(-6,-6)).to.equal(-12)
+});
+it("adds positive number to negative number", () =>{
+    expect(sum(0,-6)).to.equal(-6)
+});
+it('should return NaN if passed a word string', function() {
+    expect(sum( "hello", "pal" )).to.be.NaN;
+  });
 })
