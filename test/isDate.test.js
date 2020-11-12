72079ccd5e3684c65c2de2ce3563052e4ca8ece7
shamshad akther <shamshad.akther@tuni.fi>
2020-11-12 19:30:48 +0200

    file name correction

--- test/isDate.test.js
+++ test/isDate.test.js
@@ -1,17 +1,17 @@
 // node 12 and up
 import chai from "chai"
-import isdate from "../src/isdate.js"
+import isDate from "../src/isDate.js"
 const expect = chai.expect
 
 
-describe("isdate Operation", () => {
-    it("if value isdate object should return true", () =>{
-        expect(isdate(new Date)).to.equal(true)
+describe("isDate Operation", () => {
+    it("if value isDate object should return true", () =>{
+        expect(isDate(new Date)).to.equal(true)
     });
     it("if value is not date object should return false", () =>{
-        expect(isdate('Mon April 23 2012')).to.equal(false)
+        expect(isDate('Mon April 23 2012')).to.equal(false)
     });
     it("if value is null should return false", () =>{
-        expect(isdate(null)).to.equal(false)
+        expect(isDate(null)).to.equal(false)
     });
 })
